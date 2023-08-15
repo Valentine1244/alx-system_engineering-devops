80661db08a0165b871bef386fb82b746097728c7
Valentine <akamigboifeanyi22@gmail.com>
2023-08-15 01:22:55 -0700

    2-repetition_token_1.rb

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,5 +1,3 @@
 #!/usr/bin/env ruby
-# Displays a list of currently running processes containing the bash keyword.
-
-ps -aux | grep bash
-
+# checks for matching cases
+puts ARGV[0].scan(/hb?tn/).join
